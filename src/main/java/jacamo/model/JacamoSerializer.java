90455f2629954e557a79cb7158b62781eb9e7e08
Samuele <samuele.bura98@gmail.com>
2021-10-13 13:42:39 +0200

    Fix serializing error

--- src/main/java/jacamo/model/JacamoSerializer.java
+++ src/main/java/jacamo/model/JacamoSerializer.java
@@ -3,13 +3,13 @@ package jacamo.model;
 public class JacamoSerializer {
 
   public static String toJacamoString(AgentDefinition def){
-      return def.getName() + " : " + def.getType() + ".asl\n";
+      return "\tagent "+def.getName() + " : " + def.getType() + ".asl\n";
   }
 
   public static String toJacamoString(MasDefinition def) {
     return def.getAgents().stream()
       .map(JacamoSerializer::toJacamoString)
       .reduce("mas "+def.getId()+" {\n", (s, n) -> s+n)
-      .concat("\n}\n");
+      .concat("}\n");
   }
 }
